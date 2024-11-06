0f7229a4214f4417df1a13097fcab22f1cd841f4
subrainion <145193806+subrainion@users.noreply.github.com>
2024-11-06 07:31:04 +0500

    Add files via upload

--- lab3/lab3.cpp
+++ lab3/lab3.cpp
@@ -6,72 +6,62 @@
 using namespace std;
 class human //zadanie 1.2
 {
-public:
 	string name;
 	int height;
-	void print();
+public:
+	human(string name, int height) : name(name), height(height) {}
+	int getheight() { return height; }
+	void print()
+	{
+		cout << name << ',' << " рост: " << height << endl;
+	}
 };
-void human::print()
-{
-	cout << name << ',' << " рост: " << height << endl;
-}
 class names //zadanide 1.3
 {
-public:
 	string surname;
 	string name;
 	string patronymic;
-	void print();
-};
-void names::print()
-{
-	if (surname.empty())
-		cout << name << patronymic << endl;
-	if (patronymic.empty())
-		cout << surname << name << endl;
-	else
-		cout << surname << name << patronymic << endl;
-}
-class humanwithname //zadanide 2.2
-{
 public:
-	string name;
-	int height;
-
-	humanwithname(string name, int height) : name(name), height(height) {}
-
-	string ToString() const {
-		return name + ", рост: " + to_string(height);
+	names(string surname, string name, string patronymic) : surname(surname), name(name), patronymic(patronymic) {}
+	string print()
+	{
+		string result;
+		if (!surname.empty())
+			result += surname + " ";
+		if (!name.empty())
+			result += name + " ";
+		if (!patronymic.empty())
+			result += patronymic;
+		return result;
 	}
 };
-struct Путь { //zadanide 3
-	string Город;
-	int Стоимость;
+struct way { //zadanide 3
+	string city;
+	int cost;
 
-	Путь(string город, int стоимость) : Город(город), Стоимость(стоимость) {}
+	way(string city, int cost) : city(city), cost(cost) {}
 };
 class cities
 {
+	string name;
+	vector<way> ways;
 public:
-	string Название;
-	vector<Путь> Пути;
-	void print();
+	cities(string name) : name(name) {}
 
-	cities(string название) : Название(название) {}
-
-	void ДобавитьПуть(string город, int стоимость) 
+	void addway(string city, int cost) 
 	{
-		Пути.push_back(Путь(город, стоимость));
+		ways.push_back(way(city, cost));
+	}
+	void print()
+	{
+		cout << "Город: " << name << endl;
+		cout << "Связанные города:" << endl;
+		for (const auto& way : ways)
+		{
+			cout << "  " << way.city << ": " << way.cost << endl;
+		}
 	}
 };
-void cities::print()
-{
-	cout << "Город: " << Название << endl;
-	cout << "Связанные города:" << endl;
-	for (const auto& путь : Пути) {
-		cout << "  " << путь.Город << ": " << путь.Стоимость << endl;
-	}
-}
 int main()
 {
 	SetConsoleCP(1251);
@@ -79,46 +69,60 @@ int main()
 	int choice;
 	cout << "Введите номер задания (1-5):";
 	cin >> choice;
-	//1.2
-	human Cleopatra{ "Клеопатра", 152 };
-	human Pushkin{ "Пушкин", 167 };
-	human Vladimir{ "Владимир", 189 };
-	//1.3
-	names Cleopatr{ "Клеопатра"};
-	names Pushki{ "Пушкин ", "Александр ", "Сергеевич"};
-	names Vladimi{ "Маяковский ", "Владимир"};
 	//3
-	cities A{ "A" };
-	cities B{ "B" };
-	cities C{ "C" };
-	cities D{ "D" };
-	cities E{ "E" };
-	cities F{ "F" };
-	A.ДобавитьПуть("B", 5);
-	A.ДобавитьПуть("F", 1);
-	A.ДобавитьПуть("D", 6);
-	B.ДобавитьПуть("A", 5);
-	B.ДобавитьПуть("C", 3);
-	C.ДобавитьПуть("B", 3);
-	C.ДобавитьПуть("D", 4);
-	D.ДобавитьПуть("C", 4);
-	D.ДобавитьПуть("A", 6);
-	D.ДобавитьПуть("E", 2);
-	E.ДобавитьПуть("F", 2);
-	F.ДобавитьПуть("E", 2);
 	switch (choice)
 	{
 	case 1:
+		{
+		human Cleopatra("Клеопатра", 152);
+		human Pushkin("Пушкин", 167);
+		human Vladimir("Владимир", 189);
+		cout << "1.2" << endl;
 		Cleopatra.print();
 		Pushkin.print();
 		Vladimir.print();
-		Cleopatr.print();
-		Pushki.print();
-		Vladimi.print();
+		cout << "1.3" << endl;
+		names Cleopatr("", "Клеопатра", "");
+		names Pushki("Пушкин", "Александр", "Сергеевич");
+		names Vladimi("Маяковский", "Владимир", "");
+		cout << Cleopatr.print() << endl;
+		cout << Pushki.print() << endl;
+		cout << Vladimi.print();
 		break;
+		}
 	case 2:
+		{
+		names Cleopatra("", "Клеопатра", "");
+		names Pushkin("Пушкин", "Александр", "Сергеевич");
+		names Vladimir("Маяковский", "Владимир", "");
+		human Cle(Cleopatra.print(), 152);
+		human Pu(Pushkin.print(), 167);
+		human Vla(Vladimir.print(), 189);
+		Cle.print();
+		Pu.print();
+		Vla.print();
 		break;
+		}
 	case 3:
+		{
+		cities A{ "A" };
+		cities B{ "B" };
+		cities C{ "C" };
+		cities D{ "D" };
+		cities E{ "E" };
+		cities F{ "F" };
+		A.addway("B", 5);
+		A.addway("F", 1);
+		A.addway("D", 6);
+		B.addway("A", 5);
+		B.addway("C", 3);
+		C.addway("B", 3);
+		C.addway("D", 4);
+		D.addway("C", 4);
+		D.addway("A", 6);
+		D.addway("E", 2);
+		E.addway("F", 2);
+		F.addway("E", 2);
 		A.print();
 		B.print();
 		C.print();
@@ -126,10 +130,33 @@ int main()
 		E.print();
 		F.print();
 		break;
+		}
 	case 4:
+	{
+		string name, link;
+		int cost, count;
+		cout << "Введите название города: ";
+		cin >> name;
+		cities A(name);
+		cout << "Введите количество связанных городов(введите '0'  для пропуска): ";
+		cin >> count;
+		if (count == 0)
+		{
+			A.print();
+			break;
+		}
+		for (int i = 0; i != count; i++)
+		{
+			cout << "Введите связанный город и стоимость пути: ";
+			cin >> link >> cost;
+			A.addway(link, cost);
+		};
+		A.print();
 		break;
+		}
 	case 5:
-
+		{
 		break;
+		}
 	}
 }
