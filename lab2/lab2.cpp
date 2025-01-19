43ffc21247ebd63dea512aded081db992887e99b
subrainion <145193806+subrainion@users.noreply.github.com>
2025-01-20 03:10:01 +0500

    Add files via upload

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -4,6 +4,7 @@
 #include <deque>
 #include <list>
 #include <forward_list>
+#include <algorithm>
 #include <Windows.h>
 using namespace std;
 template <typename Container>
@@ -12,6 +13,11 @@ void input(Container& container)
     int size;
     cout << "Введите количество элементов: ";
     cin >> size;
+    if (size < 1)
+    {
+        cout << "Неверное количество элементов";
+        exit(1);
+    }
     cout << "Введите элементы: ";
     for (int i = 0; i < size; ++i)
     {
@@ -47,6 +53,47 @@ void insertmid(list<int>& L1, list<int>& L2) //Задание 3
     auto mid = L1.begin();
     advance(mid, L1.size() / 2);
     L2.splice(L2.end(), L1, mid, next(mid));
+}
+void printEvenIndexedNumbers() //zadanie 4
+{
+    cout << "Введите набор вещественных чисел (для завершения ввода введите нечисловое значение):" << endl;
+
+    // Используем istream_iterator для чтения чисел из стандартного ввода
+    istream_iterator<double> start(cin), end;
+
+    int index = 0;
+
+    // Лямбда-выражение для фильтрации четных индексов
+    auto is_even_index = [&index](double) mutable {
+        return (index++ % 2) != 1; // Возвращаем true для нечетных индексов
+        };
+
+    // Используем remove_copy_if для копирования только четных индексов в выходной поток
+    remove_copy_if(start, end, ostream_iterator<double>(cout, " "), is_even_index);
+}
+void remove_first_and_last_zero(list<int>& L) //zadanide 5
+{
+    auto first_zero = find(L.begin(), L.end(), 0);
+    if (first_zero == L.end()) {
+        return;
+    }
+
+    auto last_zero = find(L.rbegin(), L.rend(), 0);
+
+    if (first_zero == (--last_zero.base())) {
+        L.erase(first_zero);
+        return;
+    }
+    L.erase(first_zero);
+    L.erase((++last_zero).base());
+}
+void processD(deque<int>& D) //zadanide 6
+{
+    deque<int> D0;
+    for (size_t i = 0; i < D.size() / 2; ++i) {
+        D0.push_back(D[i]);
+    }
+    replace_copy_if(D0.rbegin(), D0.rend(), back_inserter(D), [](int x) { return x < 0; }, 0);
 }
 int main()
 {
@@ -94,6 +141,7 @@ int main()
         cout << "Измененный список:" << endl;
         print(L); break;
     case 2:
+        cout << "Количество элементов вектора должно быть четным" << endl;
         cout << "Ввод данных для вектора:" << endl;
         input(V);
         if ((V.size() % 2 != 0) || (V.size() < 2))
@@ -105,6 +153,7 @@ int main()
         print(V); 
         break;
     case 3:
+        cout << "Количество элементов списка L1 должно быть нечетным" << endl;
         cout << "Ввод данных для списка L1:" << endl;
         input(L);
         if ((L.size() % 2 == 0) || (L.size() < 0))
@@ -119,6 +168,39 @@ int main()
         print(L);
         cout << "Измененный список L2:" << endl;
         print(L2);
+        break;
+    case 4:
+    {
+        printEvenIndexedNumbers();
+        break;
+    }
+    case 5:
+    {
+        input(L);
+        remove_first_and_last_zero(L);
+        print(L);
+        break;
+    }
+    case 6:
+    {
+        cout << "Количество элементов должно быть четным." << endl;
+        input(D);
+        if (D.size() % 2 != 0)
+        {
+            cout << "Нечетное количество элементов." << endl;
+            break;
+        }
+        processD(D);
+        print(D);
+        break;
+    }
+    case 7:
+        break;
+    case 8:
+        break;
+    case 9:
+        break;
+    case 10:
         break;
     }
 }
